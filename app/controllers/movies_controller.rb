fbfac6cb93487feac9f32e022bdd7e3a3721e500
kheangtang <ktang1@uncc.edu>
2016-03-02 22:47:11 +0000

    Done with section 4.4

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,9 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+private
+def movie_params
+  params.require(:movie).permit(:title, :rating, :description, release_date)
+end
 end
