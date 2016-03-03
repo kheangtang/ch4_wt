449ae0e3562c616ed72c51ff3ccab81df9908e4b
kheangtang <ktang1@uncc.edu>
2016-03-03 00:16:05 +0000

    Done with section 4.6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,6 +14,13 @@ def new
   # default: render 'new' template
 end
 
+# in movies_controller.rb
+def create
+  @movie = Movie.create!(params[:movie])
+  flash[:notice] = "#{@movie.title} was successfully created."
+  redirect_to movies_path
+end
+
 private
 def movie_params
   params.require(:movie).permit(:title, :rating, :description, release_date)
